d5394253663ff6bb9c733f5697957066d6966a33
Zettex <safaandsafa4@gmail.com>
2024-11-18 00:00:33 +0600

    requirements.txt file added

--- Authentication/gs11/gs11/asgi.py
+++ Authentication/gs11/gs11/asgi.py
@@ -3,7 +3,7 @@ import app.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs8.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs11.settings')
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
